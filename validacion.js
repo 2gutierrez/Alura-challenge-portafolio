61fd0c45a4f0079f2c2d23d3807fa3eeaa688496
2gutierrez <159669831+2gutierrez@users.noreply.github.com>
2024-06-08 12:19:15 -0700

    Update validacion.js

--- validacion.js
+++ validacion.js
@@ -1,18 +1,60 @@
-// JavaScript code for form validation or any interactivity
+const campoTexto = document.querySelector("#textoIntroducido");
+const resultado = document.querySelector("#resultado");
+const errorMessage = document.querySelector("#error-message");
 
-// Form validation example
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const asunto = document.getElementById('asunto').value;
-    const mensaje = document.getElementById('mensaje').value;
+const matriz = [
+  ["e", "enter"],
+  ["i", "imes"],
+  ["a", "ai"],
+  ["o", "ober"],
+  ["u", "ufat"]
+];
 
-    if (name === '' || email === '' || asunto === '' || mensaje === '') {
-        alert('Todos los campos son obligatorios');
-    } else {
-        // Perform form submission (e.g., send data to server or display a success message)
-        alert('Mensaje enviado correctamente');
-        this.reset();
+function botonE() {
+  const inputText = campoTexto.value;
+  if (/[A-Z]/.test(inputText) || /[^a-z\s]/.test(inputText)) {
+    errorMessage.textContent = "Error: No usar letras mayúsculas ni caracteres especiales.";
+    errorMessage.style.display = "block";
+    return;
+  }
+  errorMessage.style.display = "none";
+  const texto = encriptar(inputText);
+  resultado.value = texto;
+}
+
+function encriptar(fraseEncriptada) {
+  for (let i = 0; i < matriz.length; i++) {
+    if (fraseEncriptada.includes(matriz[i][0])) {
+      fraseEncriptada = fraseEncriptada.replaceAll(matriz[i][0], matriz[i][1]);
     }
-});
+  }
+  return fraseEncriptada;
+}
+
+function botonD() {
+  const inputText = campoTexto.value;
+  if (/[A-Z]/.test(inputText) || /[^a-z\s]/.test(inputText)) {
+    errorMessage.textContent = "Error: No usar letras mayúsculas ni caracteres especiales.";
+    errorMessage.style.display = "block";
+    return;
+  }
+  errorMessage.style.display = "none";
+  const texto = desencriptar(inputText);
+  resultado.value = texto;
+}
+
+function desencriptar(fraseDesEncriptada) {
+  for (let i = 0; i < matriz.length; i++) {
+    if (fraseDesEncriptada.includes(matriz[i][1])) {
+      fraseDesEncriptada = fraseDesEncriptada.replaceAll(matriz[i][1], matriz[i][0]);
+    }
+  }
+  return fraseDesEncriptada;
+}
+
+function botonC() {
+  const copiarTexto = document.getElementById("resultado");
+  copiarTexto.select();
+  document.execCommand("copy");
+  alert("Texto copiado al portapapeles");
+}
